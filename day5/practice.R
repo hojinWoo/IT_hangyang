e359e2d9a7e7f159b5fec76e320855b45fec930d
JINSUNG\dnghwls7 <dnghwls7@ajou.ac.kr>
2019-05-04 13:41:52 +0900

    day5 practice by R

--- day5/practice.R
+++ day5/practice.R
@@ -40,11 +40,73 @@ class(result) #ovun.sample
 str(result)
 class(result$data)  #data.frame
 str(result$data)
-trainData.bal <- ovun.sample(y~., data = trainData, p=0.5, seed=1, method="both")$data
-table(trainData.bal$y)  #균형을 잡았기 때문에 비슷하게 되도록 설정
-View(trainData.bal)
-bank_ctree_bal <- ctree(y~., data=trainData.bal, control = ctree_control(maxdepth = 3)) #모델 생성
+trainData_bal <- ovun.sample(y~., data = trainData, p=0.5, seed=1, method="both")$data
+table(trainData_bal$y)  #균형을 잡았기 때문에 비슷하게 되도록 설정
+View(trainData_bal)
+bank_ctree_bal <- ctree(y~., data=trainData_bal, control = ctree_control(maxdepth = 3)) #모델 생성
 testPred_bal <- predict(bank_ctree_bal, newdata = testData)
 table(testData$y, testPred_bal)
+confusionMatrix(testPred, testData$y, positive ="yes",mode="prec_recall")
+confusionMatrix(testPred_bal, testData$y, positive ="yes",mode="prec_recall")
+#성능 체크
+
+## 통신데이터
+rm(list = ls())
+churn_train <- read.csv("data/churnTrain.csv")
+churn_test <- read.csv("data/churnTest.csv")
+str(churn_train)
+# View(churn_train)
+str(churn_test)
+bank_ctree <- ctree(churn~., data=churn_train, control = ctree_control(maxdepth = 3))
+testPred <- predict(bank_ctree, newdata = churn_test)
+table(churn_test$churn, testPred)
+traindata_bal <- ovun.sample(churn~., data = churn_train, p=0.5, seed=1, method="both")$data
+bank_ctree_bal <- ctree(churn~., data=traindata_bal, control = ctree_control(maxdepth = 3))
+testPred_bal <- predict(bank_ctree_bal, newdata = churn_test)
+table(churn_test$churn, testPred_bal)
+confusionMatrix(testPred, churn_test$churn, positive ="yes",mode="prec_recall")
+confusionMatrix(testPred_bal, churn_test$churn, positive ="yes",mode="prec_recall")
+
+
+## R로 그리는 인공신경망 기본 실습
+rm(list = ls())
+set.seed(1234)
+ind <- sample(2, nrow(iris), replace = T, prob = c(0.7, 0.3))
+trainData <- iris[ind==1, ]
+testData <- iris[ind==2, ]
+install.packages("nnet")
+library(nnet) #single neural network
+model.nnet <- nnet(Species~., data=trainData, size = 2, decay = 5e-04)
+summary(model.nnet)
+testPred <- predict(model.nnet, testData, type="class")
+table(testPred, testData$Species)
+
+
+## 실습
+rm(list = ls())
+bank = read.csv("data/bank-full.csv", header = T, sep = ";", quote = "\"");
+set.seed(1234)
+ind <- sample(2, nrow(bank), replace = T, prob = c(0.7, 0.3))
+trainData  <- bank[ind == 1, ]
+testData <- bank[ind == 2, ]
+bank_nnet <- nnet(y~., data = trainData, size = 4, decay=5e-4)
+testPred <- predict(bank_nnet, testData, type="class")
+table(testPred, testData$y)
+trainData_bal <- ovun.sample(y~., data = trainData, p=0.5, seed=1, method="both")$data
+bank_nnet_bal <- nnet(y~., data = trainData_bal, size = 4, decay=5e-4)
+testPred_bal <- predict(bank_nnet_bal, testData, type="class")
+table(testPred_bal, testData$y)
+#str(factor(testPred))
+#as.factor(testPred)
+str(testData$y)
+bank_ctree <- ctree(y~., data=trainData, control = ctree_control(maxdepth = 3))
+DTtestPred <- predict(bank_ctree, newdata = testData)
+bank_ctree_bal <- ctree(y~., data=trainData_bal, control = ctree_control(maxdepth = 3))
+DTtestPred_bal <- predict(bank_ctree_bal, newdata = testData)
+confusionMatrix(as.factor(testPred), testData$y, positive ="yes",mode="prec_recall")
+confusionMatrix(DTtestPred, testData$y, positive ="yes",mode="prec_recall")
+confusionMatrix(as.factor(testPred_bal), testData$y, positive ="yes",mode="prec_recall")
+confusionMatrix(DTtestPred_bal, testData$y, positive ="yes",mode="prec_recall")
+
 
 
